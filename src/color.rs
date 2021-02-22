6ca43051dd17cf6cd6379d4cc96b3be48dba2d08
Josh Sanchez <joshuaesanch@gmail.com>
2021-02-21 18:57:26 -0600

    Color constructor

--- src/color.rs
+++ src/color.rs
@@ -1,3 +1,15 @@
 use nalgebra::Vector3;
 
-type Color = Vector3<i32>;
+pub type Color = Vector3<i32>;
+
+pub struct Material {
+    base_color: Color
+}
+
+impl Material {
+    pub fn new(r: i32, g: i32, b: i32) -> Material {
+        Material {
+            base_color: Color::new(r, g, b)
+        }
+    }
+}
