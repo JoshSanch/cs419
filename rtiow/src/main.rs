49d63bdb663554f178fd35fe89b3bf93b5adcba5
Joshua Sanchez <joshuaesanch@gmail.com>
2021-03-06 15:30:14 -0600

    Migrate main to utilize Camera struct

--- rtiow/src/main.rs
+++ rtiow/src/main.rs
@@ -4,11 +4,10 @@ mod utils;
 mod renderables;
 mod renderutil;
 
-use nalgebra::Vector3;
-
 use renderutil::Point3;
 use renderutil::Ray;
 use renderutil::HittableList;
+use renderutil::Camera;
 use renderables::Sphere;
 use shading::calc_ray_color;
 use imgrender::render_image;
@@ -22,20 +21,13 @@ fn main() {
     // World
     let mut world = HittableList::new();
     let sphere1 = Box::new(Sphere::new(Point3::new(0., 0., -1.,), 0.5));
-    let sphere2 = Box::new(Sphere::new(Point3::new(0., 0., -1.,), 0.5));
+    let sphere2 = Box::new(Sphere::new(Point3::new(0., -100.5, -1.,), 100.));
 
     world.add(sphere1);
     world.add(sphere2);
 
     // Camera
-    let viewport_height = 2.0;
-    let viewport_width = aspect_ratio * viewport_height;
-    let focal_length = 1.0;
-
-    let origin = Point3::new(0., 0., 0.);
-    let horizontal = Vector3::new(viewport_width, 0., 0.);
-    let vertical = Vector3::new(0., viewport_height, 0.);
-    let lower_left_corner = origin - horizontal / 2. - vertical / 2. - Vector3::new(0., 0., focal_length);
+    let cam = Camera::new();
 
     // Render
     println!("P3\n{} {} \n255\n", image_width, image_height);
@@ -46,7 +38,7 @@ fn main() {
         for x in 0..image_width {
             let u = x as f64 / (image_width-1) as f64;
             let v = y as f64 / (image_height-1) as f64;
-            let ray = Ray::new(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+            let ray = cam.calc_ray(u, v);
             let pixel_color = calc_ray_color(&ray, &world);
 
             pixel_colors.push(pixel_color.x);
