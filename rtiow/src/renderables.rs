976ec2e36dbe4058dfe6bcb6392833b6b7b8686b
Josh Sanchez <joshuaesanch@gmail.com>
2021-03-07 18:28:01 -0600

    Add Triangle renderable with Hittable impl

--- rtiow/src/renderables.rs
+++ rtiow/src/renderables.rs
@@ -83,3 +83,66 @@ impl Hittable for Plane {
         return true;
     }
 }
+
+pub struct Triangle {
+    point_one: Point3,
+    point_two: Point3,
+    point_three: Point3,
+}
+
+impl Hittable for Triangle {
+    fn hit(&self, ray: &Ray, t_min: f64, t_max: f64, record: &mut HitRecord) -> bool {
+        // Adapted from https://en.wikipedia.org/wiki/M%C3%B6ller%E2%80%93Trumbore_intersection_algorithm
+        let edge1 = self.point_two - self.point_one;
+        let edge2 = self.point_three - self.point_one;
+        let h = ray.dir.cross(&edge2);
+        let a = edge1.dot(&h);
+
+        if a == 0. {
+            return false;
+        }
+
+        let f = 1.0 / a;
+        let s = ray.orig - self.point_one;
+        let u = f * s.dot(&h);
+
+        if u < 0.0 || u > 1.0 {
+            return false;
+        }
+
+        let q = s.cross(&edge1);
+        let v = f * ray.dir.dot(&q);
+
+        if v < t_min || u + v > 1.0 {
+            return false;
+        } else {
+            record.hit_time = f * edge2.dot(&q);
+            record.hitpt = ray.find_pos_at(record.hit_time);
+            record.set_face_normal(&ray, &self.calc_surface_normal());
+
+            return true;
+        }
+    }
+}
+
+impl Triangle {
+    pub fn new(pt1: Point3, pt2: Point3, pt3: Point3) -> Triangle {
+        Triangle {
+            point_one: pt1,
+            point_two: pt2,
+            point_three: pt3
+        }
+    }
+
+    pub fn calc_surface_normal(&self) -> Vector3<f64> {
+        // Adapted from https://www.khronos.org/opengl/wiki/Calculating_a_Surface_Normal
+        let u = self.point_two - self.point_one;
+        let v = self.point_three - self.point_one;
+
+        let nx = u.y * v.z - u.z * v.y;
+        let ny = u.z * v.x - u.x * v.z;
+        let nz = u.x * v.y - u.y * v.x;
+
+        Vector3::new(nx, ny, nz)
+    }
+}
