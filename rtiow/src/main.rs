9a94579e9307dd02a98779751779190ee61fc729
Josh Sanchez <joshuaesanch@gmail.com>
2021-03-07 18:28:25 -0600

    Update main with Triangle renderable

--- rtiow/src/main.rs
+++ rtiow/src/main.rs
@@ -4,11 +4,18 @@ mod utils;
 mod renderables;
 mod renderutil;
 
+use nalgebra::Vector3;
+
 use renderutil::Point3;
 use renderutil::Ray;
 use renderutil::HittableList;
 use renderutil::Camera;
+use renderutil::CameraMode;
+
 use renderables::Sphere;
+use renderables::Plane;
+use renderables::Triangle;
+
 use shading::calc_ray_color;
 use imgrender::render_image;
 
@@ -21,13 +28,19 @@ fn main() {
     // World
     let mut world = HittableList::new();
     let sphere1 = Box::new(Sphere::new(Point3::new(0., 0., -1.,), 0.5));
-    let sphere2 = Box::new(Sphere::new(Point3::new(0., -100.5, -1.,), 100.));
+    let plane = Box::new(Plane::new(Point3::new(0., -2.0, 0.), Vector3::new(0., -1., 0.)));
+    let triangle = Box::new(Triangle::new(
+        Point3::new(0., 0., -5.),
+        Point3::new(0., -4., -4.),
+        Point3::new(2., 0., -4.)
+    ));
 
     world.add(sphere1);
-    world.add(sphere2);
+    world.add(plane);
+    world.add(triangle);
 
     // Camera
-    let cam = Camera::new();
+    let cam = Camera::new(Point3::new(-2., 2., 1.), Point3::new(0., 0., -1.), Vector3::new(0., 1., 0.), 90., aspect_ratio);
 
     // Render
     println!("P3\n{} {} \n255\n", image_width, image_height);
@@ -38,7 +51,7 @@ fn main() {
         for x in 0..image_width {
             let u = x as f64 / (image_width-1) as f64;
             let v = y as f64 / (image_height-1) as f64;
-            let ray = cam.calc_ray(u, v);
+            let ray = cam.calc_ray(u, v, &CameraMode::Orthographic);
             let pixel_color = calc_ray_color(&ray, &world);
 
             pixel_colors.push(pixel_color.x);
